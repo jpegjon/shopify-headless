e812db49a2419e01853edb1575d0d411dcce2311
Jon Tsikniadopoulos <jon.tsiknias@gmail.com>
2022-01-13 20:12:07 -0800

    add top-banner

--- components/Nav.js
+++ components/Nav.js
@@ -1,34 +1,37 @@
-import Link from 'next/link'
-import { useContext } from 'react'
-import { CartContext } from '../context/shopContext'
-import MiniCart from './MiniCart'
+import Link from "next/link";
+import { useContext } from "react";
+import { CartContext } from "../context/shopContext";
+import MiniCart from "./MiniCart";
 
 export default function Nav() {
-  const { cart, cartOpen, setCartOpen } = useContext(CartContext)
+  const { cart, cartOpen, setCartOpen } = useContext(CartContext);
 
-  let cartQuantity = 0
-  cart.map(item => {
-    return (cartQuantity += item?.variantQuantity)
-  })
+  let cartQuantity = 0;
+  cart.map((item) => {
+    return (cartQuantity += item?.variantQuantity);
+  });
 
   return (
     <header className="border-b sticky top-0 z-20 bg-white">
+      <div className="bg-black h-10 flex justify-center items-center text-sm">
+        <span className="text-white">
+          Free U.S. ground shipping on orders over $150
+        </span>
+      </div>
       <div className="flex items-center justify-between max-w-6xl pt-4 pb-2 px-4 mx-auto lg:max-w-screen-xl">
         <Link href="/" passHref>
           <a className="cursor-pointer">
-            <span className="text-lg pt-1 font-bold">
-              Shopify + Next.js
-            </span>
+            <span className="text-lg pt-1 font-bold">Shopify + Next.js</span>
           </a>
         </Link>
-        <a 
+        <a
           className="text-md font-bold cursor-pointer"
           onClick={() => setCartOpen(!cartOpen)}
-          >
+        >
           Cart ({cartQuantity})
         </a>
         <MiniCart cart={cart} />
       </div>
     </header>
-  )
+  );
 }
